596032d71e3f84396d4a1e578c895c7d65f9666e
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-31 15:18:56 -0600

    Add progress bar component

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import BudgetActualExpected from '../components/graph'
+import ProgressBar from '../components/progressBar'
 
 export default function Profile() {
 
   // logic goes here
   //const 
+  
 
   return (
     <div class="graph">
       <BudgetActualExpected />
+      <div class="progress-container">
+        <ProgressBar currentValue={80}/> <br></br>
+        <ProgressBar currentValue={35}/> <br></br>
+        <ProgressBar currentValue={70}/> <br></br>
+      </div>
     </div>
   )
 }
