ff4ba83a66e3aa06ae3c34b97f1d0645b1c5d15d
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-31 11:38:53 -0600

    Update seed data, and naming conventions in /DB

--- server/src/db/seed/budgets.sql
+++ server/src/db/seed/budgets.sql
@@ -1,1 +1,6 @@
-INSERT INTO budgets (user_id, name, start_date) VALUES (1, 'my life', '2020-05-25');
+INSERT INTO budgets (user_id, name, start_date) 
+VALUES (1, 'August-Budget', '2020-08-01'),
+(1, 'July-Budget', '2020-07-01'),
+(1, 'June-Budget', '2020-06-01'),
+(1, 'May-Budget', '2020-04-01'),
+(2, 'August-Budget', '2020-08-01');
