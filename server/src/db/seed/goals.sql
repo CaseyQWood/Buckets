a7f5858204b986e5bf6c0f5ff2c7a5e602b9de3c
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-30 19:17:21 -0600

    Spelling fix

--- server/src/db/seed/goals.sql
+++ server/src/db/seed/goals.sql
@@ -1,3 +1,3 @@
-INSERT INTO goals(name, user_id, budegt_id, amount_to_goal, start_date, end_date)
+INSERT INTO goals(name, user_id, budget_id, amount_to_goal, start_date, end_date)
 
-VALUES("New house in Vancouver West", 1, 1, 1000000, "2021-07-15", "2021-07-30")
+VALUES('New house in Vancouver West', 1, 1, 1000000, '2021-07-15', '2021-07-30')
