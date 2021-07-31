413b5832f4d7934409c8c86088774e9bd2829a91
Mingfeng Li <lmf950122@gmail.com>
2021-07-31 23:05:55 +0000

    Add the missing end_date to budgets seed

--- server/src/db/seeds/budgets.sql
+++ server/src/db/seeds/budgets.sql
@@ -1,6 +1,6 @@
-INSERT INTO budgets (user_id, name, start_date) 
-VALUES (1, 'August-Budget', '2020-08-01'),
-(1, 'July-Budget', '2020-07-01'),
-(1, 'June-Budget', '2020-06-01'),
-(1, 'May-Budget', '2020-04-01'),
-(2, 'August-Budget', '2020-08-01');
+INSERT INTO budgets (user_id, name, start_date, end_date) 
+VALUES (1, 'August-Budget', '2020-08-01', '2020-08-31'),
+(1, 'July-Budget', '2020-07-01', '2020-07-31'),
+(1, 'June-Budget', '2020-06-01', '2020-06-30'),
+(1, 'May-Budget', '2020-05-01', '2020-05-31'),
+(2, 'August-Budget', '2020-08-01', '2020-08-30');
