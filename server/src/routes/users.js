2d1e331df1abb497a69f7284a502d6713e5c907a
Mingfeng Li <lmf950122@gmail.com>
2021-07-31 23:14:08 +0000

    Add post, put and delete endpoints for users router

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const usersRoutes = (db) => {
+  // -------------get users
   router.get("/", (req, res) => {
     db.query(
       `
@@ -11,10 +12,11 @@ const usersRoutes = (db) => {
         res.json(response.rows);
       })
       .catch((error) => {
-        console.log(error.message);
+        res.json(error.message);
       });
   });
 
+  // -------------get user by id
   router.get("/:id", (req, res) => {
     const reqParams = req.params.id;
     db.query(
@@ -23,28 +25,73 @@ const usersRoutes = (db) => {
       `
     ,[reqParams])
       .then((response) => {
-        res.json(response.rows);
+        res.json(response.rows[0]);
       })
       .catch((error) => {
-        console.log(error.message);
+        res.json(error.message);
+      });
+  });
+   // -------------add new user
+  router.post("/", (req, res) => {
+    const fistName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const email = req.body.email;
+    const profileImageUrl = req.body.profile_image_url;
+    const password = req.body.password;
+    const individualIncome = req.body.individual_income;
+ 
+    db.query(
+      `
+      INSERT INTO users(first_name, last_name, email, profile_image_url, password, individual_income)
+      VALUES($1, $2, $3, $4, $5, $6) 
+      RETURNING *;
+      `
+    ,[fistName, lastName, email, profileImageUrl, password, individualIncome])
+      .then((response) => {
+        res.json(response.rows[0]);
+      })
+      .catch((error) => {
+        res.json(error.message);
       });
   });
   
   //-------------update users
+  router.put("/:id", (req, res) => {
+    const userId = req.params.id;
+    const fistName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const email = req.body.email;
+    const profileImageUrl = req.body.profile_image_url;
+    const password = req.body.password;
+    const individualIncome = req.body.individual_income;
+ 
+    db.query(
+      `
+      UPDATE users SET first_name = $1, last_name = $2, email = $3, profile_image_url = $4, password = $5, individual_income = $6 WHERE id = $7 RETURNING *;
+      `
+    ,[fistName, lastName, email, profileImageUrl, password, individualIncome, userId])
+      .then((response) => {
+        res.json(response.rows[0]);
+      })
+      .catch((error) => {
+        res.json(error.message);
+      });
+  });
   //-------------delete users
   router.delete("/:id", (req, res) => {
     const userID = req.params.id;
     
     db.query(
       `
-      DELETE FROM users WHERE users.id = $1;
+      DELETE FROM users WHERE users.id = $1
+      RETURNING *;
       `
     ,[userID])
       .then((response) => {
-        res.json(response.rows);
+        res.json(response.rows[0]);
       })
       .catch((error) => {
-        console.log(error.message);
+        res.json(error.message);
       });
   });
   return router;
