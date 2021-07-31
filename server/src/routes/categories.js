2f523d76d56501f556cfb9ac86564271cd855d5a
MingfengLi0122 <lmf950122@gmail.com>
2021-07-31 11:51:35 -0700

    Add update and delete function to the categories route

--- server/src/routes/categories.js
+++ server/src/routes/categories.js
@@ -30,43 +30,46 @@ const categoriesRoutes = (db) => {
       });
   });
   // -------------add categories --not working properyly
-  // router.post("/", (req, res) => {
-  //   const categoriesName = req.session.name;
-  //   const budgetId = req.session.budget_id;
-  //   const spendingLimit = req.session.spending_limit;
-  //   console.log
-  //   db.query(
-  //     `
-  //     INSERT INTO categories(name, budget_id, spending_limit)
-  //     VALUES($1, $2, $3) 
-  //     RETURNING *;
-  //     `
-  //   ,[categoriesName, budgetId, spendingLimit])
-  //     .then((response) => {
-  //       res.json(response.rows);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error.message);
-  //     });
-  // });
+  router.post("/", (req, res) => {
+    const categoriesName = req.body.name;
+    const budgetId = req.body.budget_id;
+    const spendingLimit = req.body.spending_limit;
+    console.log("categoriesName-----", categoriesName);
+    console.log("req.body----",req.body);
+    db.query(
+      `
+      INSERT INTO categories(name, budget_id, spending_limit)
+      VALUES($1, $2, $3) 
+      RETURNING *;
+      `
+    ,[categoriesName, budgetId, spendingLimit])
+      .then((response) => {
+        res.json(response.rows);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  });
 
   //-------------update categories --not working properyly
-  // router.put("/:id", (req, res) => {
-  //   const reqParams = req.params.id;
-  //   const updatedName = req.session.name;
+  router.put("/:id", (req, res) => {
+    const reqParams = req.params.id;
+    const updatedName = req.session.name;
+    const updatedLimit = req.session.spending_limit;
 
-  //   db.query(
-  //     `
-  //     UPDATE categories SET name = $2 WHERE id = $1;
-  //     `
-  //   ,[reqParams, updatedName])
-  //     .then((response) => {
-  //       res.json(response.rows);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error.message);
-  //     });
-  // });
+    db.query(
+      `
+      UPDATE categories SET name = $2, spending_limit =$3 WHERE id = $1;
+      `
+    ,[reqParams, updatedName, updatedLimit])
+      .then((response) => {
+        res.json(response.rows);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  });
+
   //-------------delete categories
   router.delete("/:id", (req, res) => {
     const reqParams = req.params.id;
