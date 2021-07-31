2a68b95f084ee97914b8f9ce7634d1518f705b64
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-07-30 18:29:41 -0600

    Add user table expense table and seeding for both

---
+++ server/src/db/seed/users_seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO users VALUE(first_name, last_name, email, profile_image_url, password, individual_income)
+VALUES ('Greg', 'Greggory', 'greggy@gmail.com', 'https://196034-584727-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/05/Business-Development-Manager-Profile-Photo.jpg', 'PASSWORD', 100000);
+
+
