fbabf716ec38287653ec63f4ef437cf908292a74
casey.Q.Wood <caseywood252@gmail.com>
2021-07-30 17:41:41 -0700

    typo on user vs users

--- server/src/db/schema/budgets.sql
+++ server/src/db/schema/budgets.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS budgets CASCADE;
 
 CREATE TABLE budgets (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   name VARCHAR(255) NOT NULL,
   start_date date NOT NULL,
   end_date date
