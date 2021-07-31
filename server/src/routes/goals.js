92e07532340b821a9b68b7834038ae78b96f2ddd
Mingfeng Li <lmf950122@gmail.com>
2021-07-31 23:13:49 +0000

    Add post, put and delete endpoints for goals router

--- server/src/routes/goals.js
+++ server/src/routes/goals.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
+//-------------get goals
 const goalsRoutes = (db) => {
   router.get("/", (req, res) => {
     db.query(
@@ -11,10 +12,11 @@ const goalsRoutes = (db) => {
         res.json(response.rows);
       })
       .catch((error) => {
-        console.log(error.message);
+        res.json(error.message);
       });
   });
 
+  //-------------get goal by id
   router.get("/:id", (req, res) => {
     const reqParams = req.params.id;
     db.query(
@@ -23,28 +25,75 @@ const goalsRoutes = (db) => {
       `
     ,[reqParams])
       .then((response) => {
-        res.json(response.rows);
+        res.json(response.rows[0]);
       })
       .catch((error) => {
-        console.log(error.message);
+        res.json(error.message);
+      });
+  });
+
+  //-------------add goal
+  router.post("/", (req, res) => {
+    const goalsName = req.body.name;
+    const userId = req.body.user_id;
+    const budgetId = req.body.budget_id;
+    const amountToGoal = req.body.amount_to_goal;
+    const startDate = req.body.start_date;
+    const endDate = req.body.end_date;
+ 
+    db.query(
+      `
+      INSERT INTO goals(name, user_id, budget_id, amount_to_goal, start_date, end_date)
+      VALUES($1, $2, $3, $4, $5, $6) 
+      RETURNING *;
+      `
+    ,[goalsName, userId, budgetId, amountToGoal, startDate, endDate])
+      .then((response) => {
+        res.json(response.rows[0]);
+      })
+      .catch((error) => {
+        res.json(error.message);
       });
   });
   
   //-------------update goals
+  router.put("/:id", (req, res) => {
+    const goalId = req.params.id;
+    const goalsName = req.body.name;
+    const userId = req.body.user_id;
+    const budgetId = req.body.budget_id;
+    const amountToGoal = req.body.amount_to_goal;
+    const startDate = req.body.start_date;
+    const endDate = req.body.end_date;
+
+    db.query(
+      `
+      UPDATE goals SET name = $1, user_id = $2, budget_id = $3, amount_to_goal = $4, start_date = $5, end_date =$6 WHERE id = $7 RETURNING *;
+      `
+    ,[goalsName, userId, budgetId, amountToGoal, startDate, endDate, goalId])
+      .then((response) => {
+        res.json(response.rows[0]);
+      })
+      .catch((error) => {
+        res.json(error.message);
+      });
+  });
+
   //-------------delete goals
   router.delete("/:id", (req, res) => {
     const goalId = req.params.id;
     
     db.query(
       `
-      DELETE FROM goals WHERE goals.id = $1;
+      DELETE FROM goals WHERE goals.id = $1
+      RETURNING *;
       `
     ,[goalId])
       .then((response) => {
-        res.json(response.rows);
+        res.json(response.rows[0]);
       })
       .catch((error) => {
-        console.log(error.message);
+        res.json(error.message);
       });
   });
   return router;
