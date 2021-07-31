3ba7b55e4090fb84cc1bb96fdea598d7b9515726
casey.Q.Wood <caseywood252@gmail.com>
2021-07-30 17:27:44 -0700

    budgets schema and seed

---
+++ server/src/db/schema/budgets.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS budgets CASCADE;
+
+CREATE TABLE budgets (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES user(id) ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
+  start_date date NOT NULL,
+  end_date date
+);
