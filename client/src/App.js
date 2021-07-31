0bdd820a031a9324b5fb6543f3488f848aa55aab
casey.Q.Wood <caseywood252@gmail.com>
2021-07-30 23:25:10 -0700

    comments

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,21 @@ import {Canvas} from '@react-three/fiber'
 import Coin from './SuperMarioCoinglb'
 import {OrbitControls} from "@react-three/drei"
 
+// currently the commented out section was just for testing base 3d object dependancies ahead of time please disregard
 
 
 function App() {
   return (
     <div>
       <p>THIS IS A TEST</p>
-      <Canvas>
+      {/* <Canvas>
       <ambientLight/>
       <pointLight position={[10, 10, 10]} intensity={3} />
       <OrbitControls/>
         <Suspense>
           <Coin roughness={0.2}/>
         </Suspense>
-      </Canvas>
+      </Canvas> */}
     
     </div>
   );
