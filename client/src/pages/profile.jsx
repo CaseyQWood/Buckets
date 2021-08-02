7de2dfb4bad1bd8a229f74ceaffb37f33991ebb0
MingfengLi0122 <lmf950122@gmail.com>
2021-08-01 19:47:25 -0700

    Add chatButton and onlineChat components to the profile route

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -3,12 +3,19 @@ import BudgetActualExpected from '../components/graph'
 import ProgressBar from '../components/progressBar'
 import CategoriesProgressBar from '../components/categoriesProgressBar'
 import OnlineChat from '../components/onlineChat'
+import ChatButton from '../components/chatButton'
+
+import { useState } from 'react'
 
 export default function Profile() {
 
   // logic goes here
   //const 
-  
+  const [state, setState] = useState('noChat');
+
+  const openChat = () => {
+    setState('openChat');
+  }
 
   return (
     <div class="graph">
@@ -17,7 +24,12 @@ export default function Profile() {
         <ProgressBar currentValue={80} name={"Living Expenses"} spendLimit={'$1200.00'}/>
       </div>
 
-      <OnlineChat />
+      <div>
+        {state === 'noChat' && (
+        <ChatButton openChat={openChat} />
+        )}
+        {state === 'openChat' && <OnlineChat />}
+      </div>
     </div>
   )
 }
