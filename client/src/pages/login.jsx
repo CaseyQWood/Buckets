869e2899ed06c5fcda1885b2d09938a9d695f670
casey.Q.Wood <caseywood252@gmail.com>
2021-08-01 18:02:31 -0700

    base login and query for user once attmept to login is made

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -3,16 +3,30 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import Coin from '../3dobjects/SuperMarioCoinglb'
 import { Physics, Debug } from '@react-three/cannon'
+import axios from 'axios'
 import '../styles/login.scss'
 
 export default function Login() {
 const [mass, setMass] = useState(0)
-console.log(mass)
-
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
 const positionArray = [[1, 1, 1], [0, 1, 0], [-1, 1.5, 1], [-2, 2.5, 1], [1.9, 2.5, 1], [-2, 1, 1]]
 
-let test = positionArray.map((pos, index) => (<Coin key={index} position={pos} mass={mass}/>)) 
+console.log(email)
+console.log(password)
 
+const login = (inputEmail, inputPassword) => {
+  const url = 'http://localhost:3002/api/users'
+  axios.get(url).then((res) => {
+    console.log(res.data)
+    res.data.forEach(x => {if (x.email === inputEmail && x.password === inputPassword){
+      setMass(10)
+      sessionStorage.setItem('token', x.id)
+      setTimeout(() => {console.log('login success')}, 500)
+      console.log(x)
+    }})
+  })
+}
 
   return (
     <div class='general'>
@@ -23,7 +37,7 @@ let test = positionArray.map((pos, index) => (<Coin key={index} position={pos} m
           <Physics gravity={[0, -mass, 0]}>
             {/* <Debug> */}
               <Suspense>
-                {positionArray.map((pos, index) => (<Coin key={index} position={pos} mass={mass}/>))}
+                {positionArray.map((pos, index) => (<Coin key={index} position={pos} />))}
                 </Suspense>
               {/* </Debug> */}
             </Physics> 
@@ -35,9 +49,9 @@ let test = positionArray.map((pos, index) => (<Coin key={index} position={pos} m
       <div class='modal'> 
         <div class='login'>
           <h3>login</h3>
-          <form><input placeholder='please enter email'/></form>
-          <form><input placeholder='please enter password'/></form>
-          <button onClick={(res) => setMass(10)}>login</button>
+          <form><input placeholder='please enter email' type='text' value={email} onChange={event => setEmail(event.target.value)}/></form>
+          <form><input placeholder='please enter password' type='text' value={password} onChange={event => setPassword(event.target.value)}/></form>
+          <button onClick={(res) => login(email, password)}>login</button>
           <a href='/register' >Register</a>
         </div>
       </div>
