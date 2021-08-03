5cdcd358fa7c78134e6f910b423082d930ee633d
MingfengLi0122 <lmf950122@gmail.com>
2021-08-02 20:51:30 -0700

    Recreate ChatButton component

--- client/src/components/chatButton.jsx
+++ client/src/components/chatButton.jsx
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
-import "../styles/chatButton.scss"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/fontawesome-free-solid'
-
+import React from "react";
+import "../styles/ChatButton.scss";
 
 export default function ChatButton(props) {
-  
   return (
     <>
-    <button class="chatButton" onClick={props.openChat}>
-      Talk to the expert! <FontAwesomeIcon icon={faCoffee} />
-    </button><i class="fa-solid fa-face-tongue-money"></i>
+      <div class="wrapper screen innerdiv">
+        <div class="icon facebook">
+          <div class="tooltip">Help?</div>
+          <span>
+          <img
+            src="https://img1.wsimg.com/dc-assets/live-engage/images/chat-baloon.svg"
+            alt="Contact Us"
+          ></img>
+          </span>
+        </div>
+      </div>
     </>
-  )
+  );
 }
