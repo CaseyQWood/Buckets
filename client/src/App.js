4c8e3d56dfb5d6100bd85beb8354bb90956a8cf7
MingfengLi0122 <lmf950122@gmail.com>
2021-08-03 23:58:23 -0700

    Add NavBar to the all routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useEffect } from 'react';
 import axios from "axios";
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 import Login from './pages/login';
 import Register from './pages/register';
 import Profile from './pages/profile';
@@ -9,23 +9,26 @@ import Budget from './pages/budget';
 
 import Join from './components/Join.jsx';
 import Chat from './components/Chat.jsx';
+import Navbar from './components/Navbar.jsx';
 
 // currently the commented out section was just for testing base 3d object dependancies ahead of time please disregard
 
 
 function App() {
   return (
+    <>
     <Router>
+      <Navbar/>
+   
       <Route exact={true} path='/login' component={Login}/>
       <Route exact={true} path='/register' component={Register}/>
       <Route exact={true} path='/profile' component={Profile}/>
       <Route exact={true} path='/budget' component={Budget}/>
       
-      
-      <Route exact={true} path='/' component={Join}/>
+      <Route exact={true} path='/expertjoin' component={Join}/>
       <Route exact={true} path='/chat' component={Chat}/>
-      <Route />
     </Router>
+    </>
   );
 }
 
