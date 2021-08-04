82ea7e41c3bf52b984c4487225bb795621f612dd
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-03 22:42:50 -0600

    Add initial routes to grab categories, expenses, and totalOfExpenses for the active budget

--- server/src/routes/budgets.js
+++ server/src/routes/budgets.js
@@ -55,6 +55,64 @@ const budgetsRoutes = (db) => {
       });
   });
 
+  // Grab all categories for active budget
+  router.get('/all/categories/:id', (req, res) => {
+    const ownerId = req.params.id;
+
+    db.query(`
+      SELECT categories.name as category_name, 
+      categories.id as category_id, 
+      categories.spending_limit as spend_limit,
+      categories.name as category_name
+      FROM categories
+      JOIN budgets ON categories.budget_id = budgets.id
+      WHERE budgets.active = true AND budgets.user_id = $1
+     `, [ownerId])
+      .then(response => {
+        res.json(response.rows)
+      })
+      .catch((error) => {
+        res.json(error.message)
+      });
+  });
+
+  // Grab the total of all expenses by category for the active budget
+  router.get('/all/totalbycategory/:id', (req, res) => {
+    const ownerId = req.params.id;
+
+    db.query(`
+    SELECT categories.name, categories.id, SUM(cost)
+    FROM expenses
+    JOIN categories ON expenses.category_id = categories.id
+    JOIN budgets ON categories.budget_id = budgets.id
+    WHERE budgets.active = true AND budgets.user_id = $1
+    GROUP BY categories.id;
+     `, [ownerId])
+      .then(response => {
+        res.json(response.rows)
+      })
+      .catch((error) => {
+        res.json(error.message)
+      });
+  });
+
+  // Grab all expenses for the current budget
+  router.get('/all/expenses/id', (req, res) => {
+    const ownerId = req.params.id;
+
+    db.query(`
+    SELECT expenses.name as expense_name,
+    expenses.category_id as category_id,
+    expenses.cost  as cost,
+    expenses.amount_paid as amount_paid,
+    expenses.payee as payee
+    FROM expenses
+    JOIN categories ON categories.id = expenses.category_id
+    JOIN budgets ON budgets.id = categories.budget_id
+    WHERE budgets.active = true AND budgets.user_id = 1;
+    `)
+  })
+
   //-------------add new budget
   router.post("/", (req, res) => {
     const userId = req.body.user_id;
