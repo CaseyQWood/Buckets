e1ee509ff52165598021f7555fbede2580c0be53
MingfengLi0122 <lmf950122@gmail.com>
2021-08-05 00:08:36 -0700

    Remove embedded login form

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -4,37 +4,40 @@ import { OrbitControls } from '@react-three/drei'
 import Coin from '../3dobjects/SuperMarioCoinglb'
 import { Physics, Debug } from '@react-three/cannon'
 import axios from 'axios'
-import '../styles/login.scss'
+// import '../styles/login.scss'
 import { Redirect, useHistory } from 'react-router-dom'
+import FaceRec from '../components/FaceRec'
+import LoginForm from '../components/LoginForm'
 
 export default function Login() {
 const [mass, setMass] = useState(0)
-const [email, setEmail] = useState('')
-const [password, setPassword] = useState('')
+// const [email, setEmail] = useState('')
+// const [password, setPassword] = useState('')
 let history = useHistory()
 const positionArray = [[1, 1, 1], [0, 1, 0], [-1, 1.5, 1], [-2, 2.5, 1], [1.9, 2.5, 1], [-2, 1, 1]]
 
-console.log(email)
-console.log(password)
+// console.log(email)
+// console.log(password)
 
-const login = (inputEmail, inputPassword) => {
-  const url = 'http://localhost:3002/api/users'
+// const login = (inputEmail, inputPassword) => {
+//   const url = 'http://localhost:3002/api/users'
   
-  axios.get(url).then((res) => {
-    console.log(res.data)
-    res.data.forEach(x => {if (x.email === inputEmail && x.password === inputPassword){
-      setMass(10)
-      sessionStorage.setItem('token', x.id)
-      sessionStorage.setItem('firstName', x.first_name)
-      sessionStorage.setItem('lastName', x.last_name)
-      setTimeout(() => {history.push('/profile')}, 750)
-      console.log(x)
-      console.log("sessionStorage-----",sessionStorage)
-    }})
-  })
-}
+//   axios.get(url).then((res) => {
+//     console.log(res.data)
+//     res.data.forEach(x => {if (x.email === inputEmail && x.password === inputPassword){
+      // setMass(10)
+//       sessionStorage.setItem('token', x.id)
+//       sessionStorage.setItem('firstName', x.first_name)
+//       sessionStorage.setItem('lastName', x.last_name)
+      // setTimeout(750)
+//       console.log(x)
+//       console.log("sessionStorage-----",sessionStorage)
+//     }})
+//   })
+// }
 
   return (
+    <>
     <div className='general'>
       <div className='falling-coins'>
         <Canvas height={window.innerHeight} width={window.innerWidth}>
@@ -51,19 +54,10 @@ const login = (inputEmail, inputPassword) => {
       </div>
 
       <p>BucketUp!</p>
-
-      <div className='user-login'> 
-        <div className='login'>
-          <h3>login</h3>
-          <form><input placeholder='please enter email' type='text' value={email} onChange={event => setEmail(event.target.value)}/></form>
-          <form><input placeholder='please enter password' type='text' value={password} onChange={event => setPassword(event.target.value)}/></form>
-          <button onClick={(res) => login(email, password)}>login</button>
-          <a href='/register' >Register</a>
-        </div>
-      </div>
-
-   
-
-    </div>
+      <LoginForm />
+    </div> 
+    
+    {/* <FaceRec/> */}
+    </>
   )
 }
