842e42ee5a18d119a64f5cc42dffbafdb461b3e4
MingfengLi0122 <lmf950122@gmail.com>
2021-08-05 17:34:23 -0700

    Remove unnessary import

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useEffect } from 'react';
-import axios from "axios";
-import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './pages/login';
 import Register from './pages/register';
 import Profile from './pages/profile';
@@ -10,7 +10,7 @@ import Budget from './pages/budget';
 import Join from './components/Join.jsx';
 import Chat from './components/Chat.jsx';
 import NavBar from './components/NavBar.jsx';
-import FaceRec from './components/FaceRec';
+
 
 
 // currently the commented out section was just for testing base 3d object dependancies ahead of time please disregard
