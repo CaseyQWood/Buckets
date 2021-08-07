36b62f9b47b45fbe8851f78fdaf640bfd52cef1e
MingfengLi0122 <lmf950122@gmail.com>
2021-08-06 22:34:41 -0700

    Add more goals seeds

--- server/src/db/seeds/goals.sql
+++ server/src/db/seeds/goals.sql
@@ -1,6 +1,10 @@
 INSERT INTO goals(name, user_id, budget_id, amount_to_goal, start_date, end_date)
 
-VALUES('New house in Vancouver West', 1, 1, 1000000, '2021-07-15', '2021-07-30'),
+VALUES('New house in Vancouver West', 1, 1, 5000000, '2021-01-15', '2021-07-30'),
 ('Vacation in Bermuda', 1, 1, 25000, '2021-07-15', '2021-07-30'),
 ('Used car from 2021', 1, 1, 150000, '2021-07-15', '2021-07-30'),
-('Another Mansion', 2, 5, 2500000, '2021-08-01', '2022-08-01');
+('Another Mansion', 1, 5, 15000000, '2021-08-01', '2022-08-30'),
+('A new watch', 1, 5, 250, '2021-03-01', '2022-03-30'),
+('Game monitor', 1, 3, 600, '2021-01-01', '2020-02-01'),
+('Ipad', 2, 5, 1300, '2021-03-01', '2021-03-15'),
+('yart', 1, 1, 10000000, '2021-05-01', '2021-12-15');
