10dbf61987e91f3a7fab27a8a5ebc7246073ec51
MingfengLi0122 <lmf950122@gmail.com>
2021-08-07 15:42:37 -0700

    Remove testing scripts

--- server/src/routes/analytics.js
+++ server/src/routes/analytics.js
@@ -3,12 +3,11 @@ const router = require("express").Router();
 
 const analyticsRoutes = (db) => {
 
-  // -------------get budget by user_id
+  // -------------get budget for analytics
   router.post("/budget/:id", (req, res) => {
     const userId = req.params.id;
     const from = req.body.from;
     const to = req.body.to;
-    
 
     db.query(
       `SELECT users.first_name as first_name, users.last_name as last_name, categories.name, categories.id, SUM(cost) as total_expense, budgets.start_date as start_date, budgets.end_date as end_date, categories.spending_limit as spending_limit
@@ -28,14 +27,14 @@ const analyticsRoutes = (db) => {
       });
   });
 
-  // 
+  //-------------get goals for analytics
   router.post("/goals/:id", (req, res) => {
     const userId = req.params.id;
     const from = req.body.from;
     const to = req.body.to;
 
     db.query(
-      `SELECT goals.name, goals.amount_to_goal, goals.start_date, goals.end_date from goals WHERE goals.start_date >= $2 AND goals.end_date <= $3 AND goals.user_id = $1;
+      `SELECT goals.name, goals.amount_to_goal, goals.amount_added, goals.start_date, goals.end_date from goals WHERE goals.start_date >= $2 and goals.end_date <= $3 AND goals.user_id = $1;;
        `, [userId, from, to]
     )
       .then((response) => {
@@ -48,19 +47,5 @@ const analyticsRoutes = (db) => {
 
   return router;
 };
-
-        // analytics budeget limit and total_expense
-
-        // SELECT users.first_name as first_name, users.last_name as last_name, categories.name, categories.id, SUM(cost) as total_expense, budgets.start_date as start_date, budgets.end_date as end_date, categories.spending_limit as spending_limit
-        // FROM expenses
-        // JOIN categories ON expenses.category_id = categories.id
-        // JOIN budgets ON categories.budget_id = budgets.id
-        // JOIN users ON users.id = user_id
-        // WHERE expenses.start_date >= '2021-01-01' AND expenses.end_date <= '2021-08-30'AND users.id =1
-        // GROUP BY categories.id, budgets.id, users.id;
-
-        // analytics goal 
-        // SELECT goals.name, goals.amount_to_goal, goals.start_date, goals.end_date from goals WHERE goals.start_date >= '2021-01-01' AND goals.end_date <= '2021-08-30'AND goals.user_id =1;
-
 
 module.exports = analyticsRoutes;
