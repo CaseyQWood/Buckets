a6284869a0a9958aa9ded7621b4587e7543554d7
MingfengLi0122 <lmf950122@gmail.com>
2021-08-07 15:40:59 -0700

    Use retrived data to feed pie chart and create goals' progress bars

--- client/src/pages/Analytics.jsx
+++ client/src/pages/Analytics.jsx
@@ -1,5 +1,5 @@
 import "date-fns";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import DateFnsUtils from "@date-io/date-fns";
 import {
@@ -10,6 +10,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import BarChart from "../components/BarChart";
 import PieChart from "../components/PieChart";
 import { analyiticsContext } from "../providers/AnalyticsProvider";
+import ProgressBar from "../components/progressBar";
 
 const useStyles = makeStyles((theme) => ({
   timePicker: {
@@ -22,12 +23,15 @@ const useStyles = makeStyles((theme) => ({
 export default function Analytics() {
   // date set
   const classes = useStyles();
-  const currentDate = Date.now();
+  const toDefaultDate = Date.now();
+  const fromDaultDate = new Date(toDefaultDate);
+  fromDaultDate.setMonth(fromDaultDate.getMonth() - 1);
+
   const [selectedFromDate, setSelectedFromDate] = React.useState(
-    new Date(currentDate)
+    new Date(fromDaultDate)
   );
   const [selectedToDate, setSelectedToDate] = React.useState(
-    new Date(currentDate)
+    new Date(toDefaultDate)
   );
 
   // context import functions and state from useContext hook
@@ -63,7 +67,7 @@ export default function Analytics() {
     if (budget === undefined) return labels;
     for (let i = 0; i < budget.length; i++) {
       const date = new Date(formatDate(budget[i].start_date));
-      const month = date.toLocaleString('default', { month: 'long' });
+      const month = date.toLocaleString("default", { month: "long" });
       labels.push(month + ": " + budget[i].name);
     }
     return labels;
@@ -74,8 +78,10 @@ export default function Analytics() {
     let expenseData = [];
     if (budget === undefined) return expenseData;
     for (let i = 0; i < budget.length; i++) {
-      let intForMoney = Number(budget[i].total_expense.replace(/[^0-9.-]+/g,""));
-      expenseData.push(intForMoney)
+      let intForMoney = Number(
+        budget[i].total_expense.replace(/[^0-9.-]+/g, "")
+      );
+      expenseData.push(intForMoney);
     }
     return expenseData;
   };
@@ -85,35 +91,77 @@ export default function Analytics() {
     let spendingLimitData = [];
     if (budget === undefined) return spendingLimitData;
     for (let i = 0; i < budget.length; i++) {
-      let intForMoney = Number(budget[i].spending_limit.replace(/[^0-9.-]+/g,""));
-      spendingLimitData.push(intForMoney)
+      let intForMoney = Number(
+        budget[i].spending_limit.replace(/[^0-9.-]+/g, "")
+      );
+      spendingLimitData.push(intForMoney);
     }
     return spendingLimitData;
   };
 
-  // use retrived data from database to feed pie chart
+  // use retrived goal data from database to feed pie chart
   const goalData = (goals) => {
     let goalsData = [];
 
     if (goals === undefined) return goalsData;
     for (let i = 0; i < goals.length; i++) {
-      let intForMoney = Number(goals[i].amount_to_goal.replace(/[^0-9.-]+/g,""));
-      goalsData.push(intForMoney)
+      let intForMoney = Number(
+        goals[i].amount_to_goal.replace(/[^0-9.-]+/g, "")
+      );
+      goalsData.push(intForMoney);
     }
 
     return goalsData;
   };
 
- // set up labels for piechat
- const goalLabel = (goals) => {
+  //  use retrived added to goal data from database to feed pie chart 
+  const addedToGoalData = (goals) => {
+    let addedToGoalData = [];
+
+    if (goals === undefined) return addedToGoalData;
+    for (let i = 0; i < goals.length; i++) {
+      let intForMoney = Number(goals[i].amount_added.replace(/[^0-9.-]+/g, ""));
+      addedToGoalData.push(intForMoney);
+    }
+
+    return addedToGoalData;
+  };
+
+  // console.log("addedToGoal", addedToGoalData(goals));
+  // console.log("goalData", goalData(goals));
+
+  // set up labels for piechat
+  const goalLabel = (goals) => {
     let goalLabel = [];
     if (goals === undefined) return goalLabel;
     for (let i = 0; i < goals.length; i++) {
       goalLabel.push(goals[i].name);
     }
     return goalLabel;
- }
+  };
 
+  // percent calculate for progress
+  const percentCalculator = (num, den) => {
+    const number1 = num ? Number(num.replace(/[^0-9.-]+/g, "")) : 0.0;
+    const number2 = den ? Number(den.replace(/[^0-9.-]+/g, "")) : 0.0;
+
+    return ((number1 / number2) * 100).toFixed(2);
+  };
+
+  // generate a progress bar for each goal
+  const goalArr = goals || [];
+  const goalProgress = goalArr.map((goal, index) => {
+      const currentValue = percentCalculator(goal.amount_added, goal.amount_to_goal);
+      return (
+        <ProgressBar
+          key={index}
+          currentValue={currentValue}
+          name={goal.name}
+          spendLimit={goal.amount_to_goal}
+        />
+      );
+  })
+  
   // use retrived data from database to feed bar chart
   const data = {
     labels: budgetLabels(budget),
@@ -121,62 +169,89 @@ export default function Analytics() {
       {
         label: "Total_expense",
         data: budgetExpense(budget),
-        backgroundColor: 'rgba(75, 192, 192, 0.2)'
+        backgroundColor: "rgba(75, 192, 192, 0.2)"
       },
       {
         label: "Spending_limit",
         data: budgetSpendingLimit(budget),
-        backgroundColor: 'rgba(255, 205, 86, 0.2)'
+        backgroundColor: "rgba(255, 205, 86, 0.2)"
       }
     ]
   };
 
   // option setup for barchart
   const options = {
-    indexAxis: 'y',
+    indexAxis: "y",
     elements: {
       bar: {
-        borderWidth: 4,
-      },
+        borderWidth: 4
+      }
     },
     responsive: true,
     plugins: {
       legend: {
-        position: 'right',
+        position: "right"
       },
       title: {
-        display: true,
-        text: 'Expenses vs Spending Limit',
+        display: true
       }
     }
   };
 
+  useEffect(() => {
+    searchDate(formatDate(selectedFromDate), formatDate(selectedToDate));
+  }, []);
+
   // use retrived data from database to feed pie chart
-  const pieData = {
+  const pieGoalData = {
     labels: goalLabel(goals),
     datasets: [
       {
-        label: 'Rainfall',
+        label: "",
         backgroundColor: [
-          '#B21F00',
-          '#C9DE00',
-          '#2FDE00',
-          '#00A6B4',
-          '#6800B4',
-          '#6800B4'
+          "#C9DE00",
+          "#2FDE00",
+          "#00A6B4",
+          "#6800B4",
+          "#6800B4"
         ],
         hoverBackgroundColor: [
-        '#501800',
-        '#4B5000',
-        '#175000',
-        '#003350',
-        '#35014F',
-        '#35014F',
+          "#4B5000",
+          "#175000",
+          "#003350",
+          "#35014F",
+          "#35014F"
         ],
-        data:  goalData(goals)
+        data: goalData(goals)
       }
     ]
-  }
+  };
+
+  const pieAddedAmountData = {
+    labels: goalLabel(goals),
+    datasets: [
+      {
+        label: "",
+        backgroundColor: [
+          "#B21F00",
+          "#C9DE00",
+          "#2FDE00",
+          "#00A6B4",
+          "#6800B4",
+          "#6800B4"
+        ],
+        hoverBackgroundColor: [
+          "#501800",
+          "#4B5000",
+          "#175000",
+          "#003350",
+          "#35014F",
+          "#35014F"
+        ],
+        data: addedToGoalData(goals)
+      }
+    ]
+  };
 
   return (
     <>
@@ -221,8 +296,21 @@ export default function Analytics() {
           Search
         </button>
       </MuiPickersUtilsProvider>
-      <BarChart barData={data} options={options}/>
-      <PieChart pieData={pieData}/>
+      <div className="barchart">
+        <h3>Expenses vs Spending Limit</h3>
+        <BarChart barData={data} options={options} />
+      </div>
+
+      <div className="piechart-goal">
+        <h3>Goals</h3>
+        <PieChart pieData={pieGoalData} />
+      </div>
+
+      <div className="piechart-added-amount">
+        <h3>Added amount to goals</h3>
+        <PieChart pieData={pieAddedAmountData} />
+      </div>
+        {goalProgress}
     </>
   );
 }
