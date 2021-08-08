f615440d0cd0d3c6d0e4b90ec4bb7d90037abe58
MingfengLi0122 <lmf950122@gmail.com>
2021-08-07 22:19:02 -0700

    Fix the chat close redirect problem

--- client/src/components/InfoBar.jsx
+++ client/src/components/InfoBar.jsx
@@ -5,16 +5,20 @@ import ClearIcon from '@material-ui/icons/Clear';
 
 import '../styles/InfoBar.scss';
 
-const InfoBar = ({room}) => (
-  <div className="infoBar">
-    <div className="leftInnerContainer">
-      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
-      <div>{room}</div>
+const InfoBar = ({room, switchChatMode}) => {
+ 
+  return (
+    <div className="infoBar">
+      <div className="leftInnerContainer">
+        <img className="onlineIcon" src={onlineIcon} alt="online icon" />
+        <div>{room}</div>
+      </div>
+      <div className="rightInnerContainer">
+        <button className="chat-close-button" onClick={switchChatMode}><ClearIcon/></button>
+      </div>
     </div>
-    <div className="rightInnerContainer">
-      <ClearIcon />
-    </div>
-  </div>
-);
+  );
+}
+
 
 export default InfoBar;
