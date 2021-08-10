72589e1d184a4c7e87714393699d87eb65218883
Brandon Rowlandson <rowlandsonbrandon@gmail.com>
2021-08-10 17:41:26 -0600

    Annual Income -> monthly income

--- server/src/db/seeds/users.sql
+++ server/src/db/seeds/users.sql
@@ -1,5 +1,5 @@
 INSERT INTO users(first_name, last_name, email, profile_image_url, password, individual_income)
-VALUES ('Greg', 'Greggory', 'greggy@gmail.com', 'https://196034-584727-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/05/Business-Development-Manager-Profile-Photo.jpg', 'PASSWORD', 100000),
+VALUES ('Casey', 'Wood', 'caseywood@gmail.com', 'https://196034-584727-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/05/Business-Development-Manager-Profile-Photo.jpg', 'PASSWORD', 70000),
 ('Hugh', 'Hefner', 'playboy@gmail.com', 'https://196034-584727-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2019/05/Business-Development-Manager-Profile-Photo.jpg', 'PASSWORD', 1000000);
 
 
